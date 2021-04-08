33ea3aa640b4c80fb84ab36986ea93c9cdc3f837
DESKTOP-516SSH7\dorhi <devteam@rio-tech.com>
2021-04-08 09:29:19 +0300

    start pheonis miner (101) - on opening set afterbnurnder profil3 on close - change back to profile 1 (default)

---
+++ start_miner_dor.bat
@@ -0,0 +1,20 @@
+"C:\Mine\MSI Afterburner\MSIAfterburner.exe" -Profile3
+tasklist /FI "IMAGENAME eq PhoenixMiner.exe" 2>NUL | find /I /N "PhoenixMiner.exe">NUL
+if "%ERRORLEVEL%"=="0" (
+	echo Program is running
+) else (
+    REM
+    REM Example bat file for starting PhoenixMiner.exe to mine ETH
+    REM
+
+    setx GPU_FORCE_64BIT_PTR 0
+    setx GPU_MAX_HEAP_SIZE 100
+    setx GPU_USE_SYNC_OBJECTS 1
+    setx GPU_MAX_ALLOC_PERCENT 100
+    setx GPU_SINGLE_ALLOC_PERCENT 100
+
+    REM IMPORTANT: Replace the ETH address with your own ETH wallet address in the -wal option (Rig001 is the name of the rig)
+    PhoenixMiner.exe -pool eu-eth.ss.btc.com:443 -pool2 eu-eth.ss.btc.com:443 -wal DorEth007.101
+    pause
+	"C:\Mine\MSI Afterburner\MSIAfterburner.exe" -Profile1
+)
